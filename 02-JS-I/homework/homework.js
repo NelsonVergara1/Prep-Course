a292c87d4d5ef4f5dadf62b4e6f030d5e114642c
NelsonVergara1 <neel.vergara90@gmail.com>
2022-10-22 00:05:38 -0300

    JSI terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -67,24 +67,21 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length === str2.length) {return true}
-  else return false;
+  return str1.length === str2.length
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num < 90) {return true}
-  else return false;
+  return num < 90
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num >50) {return true}
-  else return false;
+  return num > 50
 }
 
 function obtenerResto(x, y) {
@@ -97,16 +94,14 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 === 0) { return true}
-  else return false;
+  return num % 2 === 0
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 !=0) {return true}
-  else return false;
+  return num % 2 !=0
 }
 
 function elevarAlCuadrado(num) {
@@ -212,10 +207,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (esVocal.length > 1) {return "Dato incorrecto"}
+  if (letra.length > 1) {return "Dato incorrecto"}
   else if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") { return "Es vocal"}
-  else if (letra != "a" && letra != "e" && letra != "i" && letra != "o" && letra != "u") { return "Dato incorrecto"}
-  else return false
+  else return "Dato incorrecto"
 }
 
 
